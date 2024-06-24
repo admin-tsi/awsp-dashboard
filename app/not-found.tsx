3bdd50940bd3de3f29b5de0dd62bbfece0fe4699
codebreaker02 <jordan.vitou@epitech.eu>
2024-06-24 16:38:58 +0100

    update: add not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <div className="text-center">
         <h2 className="text-4xl font-bold mb-4">404 - Not Found</h2>
         <p className="text-lg mb-8">
-          Sorry, we couldn't find the page you're looking for.
+          Sorry, we couldn&apos;t find the page you&apos;re looking for.
         </p>
         <Link
           href="/"
