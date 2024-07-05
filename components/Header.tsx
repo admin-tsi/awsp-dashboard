a54fb27fca55bb6ddfc78c13927f8a57f630cafa
codebreaker02 <jordan.vitou@epitech.eu>
2024-07-05 11:00:05 +0100

    update: add restriction for client

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,6 @@ import { UserNav } from "@/components/UserNav";
 import { SearchInput } from "@/components/Search";
 import Link from "next/link";
 import logo from "@/public/logo.svg";
-import { Button } from "@/components/ui/button";
-import { Bell, Globe } from "lucide-react";
 import { useCurrentUser } from "@/hooks/use-current-user";
 
 const Header = () => {
@@ -26,17 +24,6 @@ const Header = () => {
         <SearchInput className="hidden sm:block" />
       </div>
       <div className="flex items-center ml-auto">
-        {/*    <Button variant="outline" size="icon" className="rounded-full mr-2">
-          <Globe size={20} />
-        </Button>
-        <Button
-          variant="outline"
-          size="icon"
-          className="rounded-full mr-2 sm:mr-4"
-        >
-          <Bell size={20} />
-        </Button>*/}
-        <div className="hidden sm:block border-r-2 border-gray-200 h-6 mx-8"></div>
         <div className="flex items-center space-x-4">
           <UserNav />
           <h6 className="hidden sm:block text-md">
