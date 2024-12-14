029bcddb4bf4c9802f9c9ab0ab7a4b5d28ad9de3
codebreaker02 <jordan.vitou@epitech.eu>
2024-12-14 19:29:42 +0100

    update: with know backend

--- components/courses/steps/step3/ModuleItem.tsx
+++ components/courses/steps/step3/ModuleItem.tsx
@@ -110,6 +110,7 @@ const ModuleItem: React.FC<ModuleItemProps> = ({
     try {
       const newCourseData = {
         title_en: "New Course",
+        title_fr: "",
         description_en: "New description",
         video: "",
         courses_files: [],
